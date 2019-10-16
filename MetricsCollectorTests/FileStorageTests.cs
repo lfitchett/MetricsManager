f5d95dc39e374a88a1c8e8d1806da26c5b07f86c
Lee Fitchett <lefitche@microsoft.com>
2019-10-16 12:36:02 -0700

    make tests for file storage

---
+++ MetricsCollectorTests/FileStorageTests.cs
@@ -0,0 +1,134 @@
+using MetricsCollector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace MetricsCollectorTests
+{
+    public class FileStorageTests : TempDirectory
+    {
+        [Fact]
+        public void Storage()
+        {
+            string directory = GetTempDir();
+            FileStorage storage = new FileStorage(directory);
+
+            storage.AddScrapeResult("test_module", string.Join(", ", Enumerable.Range(0, 10)));
+
+            Assert.NotEmpty(Directory.GetFiles(Path.Combine(directory, "test_module")));
+        }
+
+        [Fact]
+        public void GetModules()
+        {
+            var testModules = Enumerable.Range(1, 10).Select(i => $"module_{i}").ToList();
+            FileStorage storage = new FileStorage(GetTempDir());
+
+            testModules.ForEach(m => storage.AddScrapeResult(m, string.Join(", ", Enumerable.Range(0, 10))));
+
+            var actual = storage.GetAllModules();
+            Assert.Equal(testModules.OrderBy(f => f), actual.OrderBy(f => f));
+        }
+
+        [Fact]
+        public void GetDataSingleEntry()
+        {
+            FileStorage storage = new FileStorage(GetTempDir());
+
+            string testData = string.Join(", ", Enumerable.Range(0, 10));
+            storage.AddScrapeResult("test_module", testData);
+
+            IDictionary<DateTime, Lazy<string>> actual = storage.GetData("test_module");
+            Assert.Single(actual);
+            Assert.Equal(testData, actual.Single().Value.Value);
+        }
+
+        [Fact]
+        public void GetDataByTime()
+        {
+            FileStorage storage = new FileStorage(GetTempDir());
+
+            storage.AddScrapeResult("test_module", "data1");
+
+            IDictionary<DateTime, Lazy<string>> actual = storage.GetData("test_module");
+            Assert.Single(actual);
+            Assert.Equal("data1", actual.Single().Value.Value);
+
+            DateTime break1 = DateTime.UtcNow;
+            storage.AddScrapeResult("test_module", "data2");
+
+            actual = storage.GetData("test_module");
+            Assert.Equal(2, actual.Count);
+            actual = storage.GetData("test_module", break1);
+            Assert.Single(actual);
+            Assert.Equal("data2", actual.Single().Value.Value);
+
+            DateTime break2 = DateTime.UtcNow;
+            storage.AddScrapeResult("test_module", "data3");
+
+            actual = storage.GetData("test_module");
+            Assert.Equal(3, actual.Count);
+            actual = storage.GetData("test_module", break1, break2);
+            Assert.Single(actual);
+            Assert.Equal("data2", actual.Single().Value.Value);
+        }
+
+        [Fact]
+        public void GetDataMany()
+        {
+            FileStorage storage = new FileStorage(GetTempDir());
+
+            /* add a variable number of scrape results to 10 different modules */
+            Dictionary<string, List<string>> testData = Enumerable.Range(1, 10)
+                .ToDictionary(i => $"module_{i}", i => Enumerable.Range(i, i).Select(j => $"test data {i}-{j}").ToList());
+            testData.ToList().ForEach(d => d.Value.ForEach(data => storage.AddScrapeResult(d.Key, data)));
+
+            foreach(var d in testData)
+            {
+                /* get all stored data and sort by timestamp (key) */
+                var actual = storage.GetData(d.Key).OrderBy(x => x.Key).Select(x => x.Value.Value);
+                Assert.Equal(d.Value, actual);
+            }            
+        }
+
+        [Fact]
+        public void RemoveOld()
+        {
+            FileStorage storage = new FileStorage(GetTempDir());
+
+            storage.AddScrapeResult("test_module", "data1");
+
+            IDictionary<DateTime, Lazy<string>> actual = storage.GetData("test_module");
+            Assert.Single(actual);
+            Assert.Equal("data1", actual.Single().Value.Value);
+
+            DateTime break1 = DateTime.UtcNow;
+            storage.AddScrapeResult("test_module", "data2");
+
+            actual = storage.GetData("test_module");
+            Assert.Equal(2, actual.Count);
+            storage.RemoveOldEntries(break1);
+            actual = storage.GetData("test_module");
+            Assert.Single(actual);
+
+            DateTime break2 = DateTime.UtcNow;
+            storage.AddScrapeResult("test_module", "data3");
+            storage.AddScrapeResult("test_module", "data4");
+            storage.AddScrapeResult("test_module", "data5");
+
+            actual = storage.GetData("test_module");
+            Assert.Equal(4, actual.Count);
+            storage.RemoveOldEntries(break2);
+            actual = storage.GetData("test_module");
+            Assert.Equal(new[] { "data3", "data4", "data5" }, actual.OrderBy(x => x.Key).Select(x => x.Value.Value));
+
+            storage.RemoveOldEntries(DateTime.UtcNow);
+            actual = storage.GetData("test_module");
+            Assert.Empty(actual);
+        }
+    }
+}
--- modules/MetricsCollector/FileStorage.cs
+++ modules/MetricsCollector/FileStorage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DotNetty.Common;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -18,34 +19,59 @@ namespace MetricsCollector
             this.directory = directory;
         }
 
-        public void WriteToFile(string module, string data)
+        public void AddScrapeResult(string module, string data)
         {
+            Directory.CreateDirectory(Path.Combine(directory, module));
             string file = Path.Combine(directory, module, DateTime.UtcNow.Ticks.ToString());
             File.WriteAllText(file, data);
         }
 
         public IEnumerable<string> GetAllModules()
         {
-            return Directory.GetDirectories(directory).Select(Path.GetDirectoryName);
+            return Directory.GetDirectories(directory).Select(Path.GetFileName);
         }
 
+        public IDictionary<DateTime, Lazy<string>> GetData(string module)
+        {
+            return GetData(module, _ => true);
+        }
+        public IDictionary<DateTime, Lazy<string>> GetData(string module, DateTime start)
+        {
+            return GetData(module, ticks => start.Ticks <= ticks);
+        }
         public IDictionary<DateTime, Lazy<string>> GetData(string module, DateTime start, DateTime end)
+        {
+            return GetData(module, ticks => start.Ticks <= ticks && ticks <= end.Ticks);
+        }
+        private IDictionary<DateTime, Lazy<string>> GetData(string module, Func<long, bool> inTimeRange)
         {
             return Directory.GetFiles(Path.Combine(directory, module))
                 .Select(Path.GetFileName)
                 .SelectWhere(fileName => (long.TryParse(fileName, out long timestamp), timestamp))
-                .Where(ticks => start.Ticks <= ticks && ticks <= end.Ticks)
+                .Where(inTimeRange)
                 .ToDictionary(
                     ticks => new DateTime(ticks),
-                    ticks => new Lazy<string>(() => File.ReadAllText(Path.Combine(directory, module, ticks.ToString())))
+                    ticks => new Lazy<string>(() =>
+                    {
+                        string file = Path.Combine(directory, module, ticks.ToString());
+                        if (File.Exists(file))
+                        {
+                            return File.ReadAllText(file);
+                        }
+                        return "";
+                    })
                 );
         }
 
         public void RemoveOldEntries(DateTime keepAfter)
         {
-            Directory.GetFiles(directory)
-                .Where(fileName => long.TryParse(fileName, out long ticks) && ticks < keepAfter.Ticks)
-                .ToList()
+            GetAllModules()
+                .SelectMany(module =>
+                    GetData(module)
+                    .Select(d => d.Key)
+                    .Where(timestamp => timestamp < keepAfter)
+                    .Select(timestamp => Path.Combine(directory, module, timestamp.Ticks.ToString()))
+                ).ToList()
                 .ForEach(File.Delete);
         }
     }
